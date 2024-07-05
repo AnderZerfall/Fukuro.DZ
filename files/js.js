b4c441682cd938f46b850d8491f6670d33ba40bf
Ander <anya.androschuk@gmail.com>
2024-07-05 21:18:37 +0300

    "blob" animation on mouse move and on click

--- files/js.js
+++ files/js.js
@@ -1,5 +1,6 @@
-// SCROLL ANIMATION PROVIDED BY ME
 'use strict'
+
+// SCROLL ANIMATION PROVIDED BY ME
 {
     let main = document.getElementById("main");             // get the scrolled main container
     let sections = document.querySelectorAll(".section");   // get all sections within the container
@@ -28,4 +29,43 @@
     }
 
     main.addEventListener('scroll', RevealSection);
+}
+// BLOB ANIMATION
+
+{
+    let blob = document.getElementById("blob");
+    let blobContainer = document.querySelector(".hero-section__logo-blob");
+    let blobSection = document.querySelector(".hero-section");
+
+    function MoveBlobByMouse(event) {
+
+        // let cursorX = (event.clientX - blob.offsetWidth) / containerSize.left * 100 - 60;
+        // let cursorY = (event.clientY - blob.offsetHeight) / containerSize.top * 100 - 100;
+        let [cursorX, cursorY] = FindCursorPosition(event);
+        
+        blob.style.transition = "all 100ms ease";
+        blob.style.transform = "translate(" + cursorX + "%," + cursorY + "%)";
+    }
+    function BlobReset() {
+        blob.style.transition = "all 500ms ease";
+        blob.style.transform = "translate(0)";
+    }
+    function FindCursorPosition(event) {
+        let containerSize = blobContainer.getBoundingClientRect();
+        let cursorX = (event.clientX - blob.offsetWidth) / containerSize.left * 100 - 60;
+        let cursorY = (event.clientY - blob.offsetHeight) / containerSize.top * 100 - 100;
+
+        return [cursorX, cursorY];
+    }
+    function ClickOnEye(event) {
+        let [cursorX, cursorY] = FindCursorPosition(event);
+        blob.style.transform = "translate("+ cursorX +"%," + cursorY + "%)" + " scaleY(0.2)";
+        setTimeout(function() {
+            blob.style.transform = "translate("+ cursorX +"%," + cursorY + "%)" + " scaleY(1)";
+        }, 150); 
+    }
+
+    blobSection.addEventListener('mousemove', MoveBlobByMouse);
+    blobSection.addEventListener('mouseleave', BlobReset);
+    window.addEventListener('click', ClickOnEye);
 }
