fdf47eba6d11974d80d7bac4de84d5622461ace2
Fukuro <anya.androschuk@gmail.com>
2024-07-10 01:36:44 +0300

    preloader animation

--- files/js.js
+++ files/js.js
@@ -1,5 +1,197 @@
 'use strict'
 
+document.addEventListener('DOMContentLoaded', () => {
+
+    // PRELOADER
+    let mainPreloader = document.querySelector(".preloader");
+    window.onload = () => {
+        setTimeout(function () {
+            mainPreloader.style.display = "none";
+        }, 100);
+    }
+    
+    // BLOB ANIMATION
+
+    {
+        let blob = document.getElementById("blob");
+        let blobContainer = document.querySelector(".hero-section__logo-blob");
+        let blobSection = document.querySelector(".hero-section");
+
+        function MoveBlobByMouse(event) {
+
+        // let cursorX = (event.clientX - blob.offsetWidth) / containerSize.left * 100 - 60;
+        // let cursorY = (event.clientY - blob.offsetHeight) / containerSize.top * 100 - 100;
+            let [cursorX, cursorY] = FindCursorPosition(event);
+        
+            blob.style.transition = "all 100ms ease";
+            blob.style.transform = "translate(" + cursorX + "%," + cursorY + "%)";
+        }
+        function BlobReset() {
+            blob.style.transition = "all 500ms ease";
+            blob.style.transform = "translate(0)";
+        }
+        function FindCursorPosition(event) {
+            let containerSize = blobContainer.getBoundingClientRect();
+            let cursorX = (event.clientX - blob.offsetWidth) / containerSize.left * 100 - 60;
+            let cursorY = (event.clientY - blob.offsetHeight) / containerSize.top * 100 - 100;
+
+            return [cursorX, cursorY];
+        }
+        function ClickOnEye(event) {
+            let [cursorX, cursorY] = FindCursorPosition(event);
+            blob.style.transform = "translate("+ cursorX +"%," + cursorY + "%)" + " scaleY(0.2)";
+            setTimeout(function() {
+                blob.style.transform = "translate("+ cursorX +"%," + cursorY + "%)" + " scaleY(1)";
+            }, 150); 
+        }
+
+        blobSection.addEventListener('mousemove', MoveBlobByMouse);
+        blobSection.addEventListener('mouseleave', BlobReset);
+        window.addEventListener('click', ClickOnEye);
+    }
+
+// SLIDER ANIMATION
+
+    {
+        let slides = document.querySelectorAll(".card");
+        let slider = document.querySelector(".slider__container");
+        let timebars = document.querySelectorAll(".card__timebar");
+        let slidesNumber = slides.length - 1;
+        let index = 0;
+        let interval = 5000;
+
+        function TimeBarAnimation(timebar) {
+            let animation = [
+                {width: "0%"},
+                {width: "95%"}
+            ];
+            let animationSettings = {
+                duration: interval,
+                itarations: 1
+            }
+
+            timebar.animate(animation, animationSettings);
+        }
+
+        function ChangeSlides() {
+
+            if (index > slidesNumber) {
+            index = 0;
+            }
+            if ((index + 1) > timebars.length - 1) {
+                TimeBarAnimation(timebars[0]);
+            }
+            else {
+                TimeBarAnimation(timebars[index + 1]);
+            }
+        
+            slides[index].style.zIndex = "-3";
+            slides[index].style.transform = "translateX(" + (slidesNumber - index) * 100 +"%)";
+
+            for (let i = 1; i <= slidesNumber; i++) {
+                if ((index + i) > slidesNumber) {
+                    for (let j = 0; j <= slidesNumber-1; j++) {
+                        slides[j].style.zIndex = "1";
+                        slides[j].style.transform = "translateX(" + (slidesNumber - index) * 120 +"%)";
+                    }
+                }   
+                else {
+                    slides[index + i].style.zIndex = "1";
+                    slides[index + i].style.transform = "translateX(-" + (index + 1) * 100 +"%)";
+                }
+            }
+            index++;
+            return index;
+        }
+
+        TimeBarAnimation(timebars[0]);
+        setInterval(ChangeSlides, interval);
+   
+    }
+
+    // CASE SECTION SCROLL
+
+    {
+        let section = document.querySelector(".cases-section__container");
+        let caseWindow = document.querySelector(".case-window");
+        let tips = document.querySelector(".cases-section__content-block");
+        let prevPos = section.scrollTop;
+
+        function HideTips(event) {
+            event.preventDefault();
+            let currentPos = section.scrollTop;
+
+            if (currentPos > prevPos) {
+                tips.style.transform = "translateY(" + 100 + "%)";
+                tips.style.opacity = 0;
+                caseWindow.style.transform = "translateY(-" + 3 +"%)";
+            }
+            else {
+                tips.style.transform = "translateY(" + 0 + "%)";
+                tips.style.opacity = 1;
+                caseWindow.style.transform = "translateY(" + 0 +"%)";
+            }
+            prevPos = currentPos;
+        }
+
+        section.addEventListener('scroll', HideTips);
+    }
+
+    // CASE SECTION PORTFOLIO BLOCK
+
+    {
+        let next = document.getElementById('next');
+        let prev = document.getElementById('prev');
+        let images = document.querySelectorAll(".case-window__img");
+        let linkText = document.querySelector(".case-window__case-name");
+        let preloader = document.querySelector(".case-window__preloader");
+        let currentImage = 0;
+
+        images[currentImage].style.display = "block";
+        linkText.innerHTML = images[currentImage].alt;
+
+        function OpenNext() {
+            preloader.style.display = "block";
+            images[currentImage].style.display = "none";
+
+            if (currentImage == images.length - 1) {
+                currentImage = 0;
+            }
+            else {
+                currentImage++;
+            }
+            RemovePreloader();
+            images[currentImage].style.display = "block";
+            linkText.innerHTML = images[currentImage].alt;
+        }
+        function OpenPrev() {
+            preloader.style.display = "block";
+            images[currentImage].style.display = "none";
+
+            if (currentImage == 0) {
+                currentImage = images.length - 1;
+            }
+            else {
+                currentImage--;
+            }
+            RemovePreloader();
+            images[currentImage].style.display = "block";
+            linkText.innerHTML = images[currentImage].alt;
+        }
+
+        next.addEventListener('click', OpenNext);
+        prev.addEventListener('click', OpenPrev);
+
+        // PRELOADER
+        function RemovePreloader() {
+            console.log("function executed");
+            setTimeout(function() {
+                preloader.style.display = "none";
+            }, 100);
+        }
+        
+    }
+});
 // SCROLL ANIMATION PROVIDED BY ME
 // {
 //     let main = document.getElementById("main");             // get the scrolled main container
@@ -31,179 +223,7 @@
 
 //     main.addEventListener('scroll', RevealSection);
 // }
-// BLOB ANIMATION
 
-{
-    let blob = document.getElementById("blob");
-    let blobContainer = document.querySelector(".hero-section__logo-blob");
-    let blobSection = document.querySelector(".hero-section");
-
-    function MoveBlobByMouse(event) {
-
-        // let cursorX = (event.clientX - blob.offsetWidth) / containerSize.left * 100 - 60;
-        // let cursorY = (event.clientY - blob.offsetHeight) / containerSize.top * 100 - 100;
-        let [cursorX, cursorY] = FindCursorPosition(event);
-        
-        blob.style.transition = "all 100ms ease";
-        blob.style.transform = "translate(" + cursorX + "%," + cursorY + "%)";
-    }
-    function BlobReset() {
-        blob.style.transition = "all 500ms ease";
-        blob.style.transform = "translate(0)";
-    }
-    function FindCursorPosition(event) {
-        let containerSize = blobContainer.getBoundingClientRect();
-        let cursorX = (event.clientX - blob.offsetWidth) / containerSize.left * 100 - 60;
-        let cursorY = (event.clientY - blob.offsetHeight) / containerSize.top * 100 - 100;
-
-        return [cursorX, cursorY];
-    }
-    function ClickOnEye(event) {
-        let [cursorX, cursorY] = FindCursorPosition(event);
-        blob.style.transform = "translate("+ cursorX +"%," + cursorY + "%)" + " scaleY(0.2)";
-        setTimeout(function() {
-            blob.style.transform = "translate("+ cursorX +"%," + cursorY + "%)" + " scaleY(1)";
-        }, 150); 
-    }
-
-    blobSection.addEventListener('mousemove', MoveBlobByMouse);
-    blobSection.addEventListener('mouseleave', BlobReset);
-    window.addEventListener('click', ClickOnEye);
-}
-
-// SLIDER ANIMATION
-
-{
-    let slides = document.querySelectorAll(".card");
-    let slider = document.querySelector(".slider__container");
-    let timebars = document.querySelectorAll(".card__timebar");
-    let slidesNumber = slides.length - 1;
-    let index = 0;
-    let interval = 5000;
-
-    function TimeBarAnimation(timebar) {
-        let animation = [
-            {width: "0%"},
-            {width: "95%"}
-        ];
-        let animationSettings = {
-            duration: interval,
-            itarations: 1
-        }
-
-        timebar.animate(animation, animationSettings);
-    }
-
-    function ChangeSlides() {
-
-        if (index > slidesNumber) {
-           index = 0;
-        }
-        if ((index + 1) > timebars.length - 1) {
-            TimeBarAnimation(timebars[0]);
-        }
-        else {
-            TimeBarAnimation(timebars[index + 1]);
-        }
-        
-        slides[index].style.zIndex = "-3";
-        slides[index].style.transform = "translateX(" + (slidesNumber - index) * 100 +"%)";
-
-        for (let i = 1; i <= slidesNumber; i++) {
-            if ((index + i) > slidesNumber) {
-                for (let j = 0; j <= slidesNumber-1; j++) {
-                    slides[j].style.zIndex = "1";
-                    slides[j].style.transform = "translateX(" + (slidesNumber - index) * 120 +"%)";
-                }
-            }
-            else {
-                slides[index + i].style.zIndex = "1";
-                slides[index + i].style.transform = "translateX(-" + (index + 1) * 100 +"%)";
-            }
-        }
-        index++;
-        return index;
-    }
-
-    TimeBarAnimation(timebars[0]);
-    setInterval(ChangeSlides, interval);
-   
-}
-
-// CASE SECTION SCROLL
-
-{
-    let section = document.querySelector(".cases-section__container");
-    let caseWindow = document.querySelector(".case-window");
-    let tips = document.querySelector(".cases-section__content-block");
-    let prevPos = section.scrollTop;
-
-    function HideTips(event) {
-        event.preventDefault();
-
-        console.log("gut");
-        console.log(section.scrollTop);
-        let currentPos = section.scrollTop;
-
-        if (currentPos > prevPos) {
-            tips.style.transform = "translateY(" + 100 + "%)";
-            tips.style.opacity = 0;
-            caseWindow.style.transform = "translateY(-" + 3 +"%)";
-            // caseWindow.scrollTo(0,43);
-        }
-        else {
-            tips.style.transform = "translateY(" + 0 + "%)";
-            tips.style.opacity = 1;
-            caseWindow.style.transform = "translateY(" + 0 +"%)";
-        }
-        prevPos = currentPos;
-    }
-
-    section.addEventListener('scroll', HideTips);
-}
-
-// CASE SECTION PORTFOLIO BLOCK
-
-{
-    let next = document.getElementById('next');
-    let prev = document.getElementById('prev');
-    let images = document.querySelectorAll(".case-window__img");
-    let linkText = document.querySelector(".case-window__case-name");
-    let currentImage = 0;
-
-    images[currentImage].style.display = "block";
-    linkText.innerHTML = images[currentImage].alt;
-
-    function OpenNext() {
-        images[currentImage].style.display = "none";
-
-        if (currentImage == images.length - 1) {
-            currentImage = 0;
-        }
-        else {
-            currentImage++;
-        }
-       
-        images[currentImage].style.display = "block";
-        linkText.innerHTML = images[currentImage].alt;
-    }
-    function OpenPrev() {
-        images[currentImage].style.display = "none";
-
-        if (currentImage == 0) {
-            currentImage = images.length - 1;
-        }
-        else {
-            currentImage--;
-        }
-
-        images[currentImage].style.display = "block";
-        linkText.innerHTML = images[currentImage].alt;
-    }
-
-    next.addEventListener('click', OpenNext);
-    prev.addEventListener('click', OpenPrev);
-}
 
 
 // FADE OUT TEXT ON THE TICKER
