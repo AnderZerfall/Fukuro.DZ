5a1d75f81d66fe2f78215b85cf85f176af6ef8ea
Fukuro <anya.androschuk@gmail.com>
2024-07-15 16:02:47 +0300

    form test

--- files/js.js
+++ files/js.js
@@ -405,36 +405,36 @@ document.addEventListener('DOMContentLoaded', () => {
         //     }
         // }
 
-        // function EmailCheck(email) {
-        //     let re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+        function EmailCheck(email) {
+            let re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
 
-        //     return re.test(email.toLowerCase());
-        // }
+            return re.test(email.toLowerCase());
+        }
 
-        // function ValidateForm(form) {
-        //     let fields = form.querySelectorAll('._req');
-        //     let hasPassed = true;
+        function ValidateForm(form) {
+            let fields = form.querySelectorAll('._req');
+            let hasPassed = true;
 
-        //     fields.forEach(field => {
-        //         if (field.value === '') {
-        //             hasPassed = false;
-        //         }
-        //         else if (field.className.includes('email') === true) {
-        //             hasPassed = EmailCheck(field.value);
-        //         }
-        //     });
+            fields.forEach(field => {
+                if (field.value === '') {
+                    hasPassed = false;
+                }
+                else if (field.className.includes('email') === true) {
+                    hasPassed = EmailCheck(field.value);
+                }
+            });
 
-        //     if (hasPassed === false) {
-        //         alert('form is empty');
-        //     }
-        // }
+            if (hasPassed === false) {
+                alert('form is empty');
+            }
+        }
 
 
-        // Pageclip.form(contactForm, {
-        //     onSubmit: ValidateForm(form),
-        //     onResponse: function (error, response) { },
-        //     successTemplate: '<span>Thank you!</span>'
-        // })
+        Pageclip.form(contactForm, {
+            onSubmit: ValidateForm(form),
+            onResponse: function (error, response) { },
+            successTemplate: '<span>Thank you!</span>'
+        })
 
         // contactForm.addEventListener('submit', ValidateForm.bind(contactForm));
     }
